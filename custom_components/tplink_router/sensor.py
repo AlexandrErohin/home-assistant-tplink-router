4134d64d332b0148d26997bee0b22d8fceb7dbd2
Luis Teixeira <teixeluis@gmail.com>
2025-10-12 12:34:49 +0100

    Revert "bugfixing, troubleshooting"
    This reverts commit 74de31edb38b8edebccc242f2a1d99d775c00315.

--- custom_components/tplink_router/sensor.py
+++ custom_components/tplink_router/sensor.py
@@ -6,7 +6,7 @@ from homeassistant.components.sensor import (
     SensorEntity,
     SensorEntityDescription,
 )
-from homeassistant.const import PERCENTAGE, SIGNAL_STRENGTH_DECIBELS_MILLIWATT, UnitOfDataRate
+from homeassistant.const import PERCENTAGE, BYTES_PER_SECOND, SIGNAL_STRENGTH_DECIBELS_MILLIWATT
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from .const import DOMAIN
@@ -15,13 +15,10 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .coordinator import TPLinkRouterCoordinator
 from tplinkrouterc6u import Status, IPv4Status, LTEStatus
 
-@dataclass
-class CombinedStatus(Status, IPv4Status, LTEStatus):
-    pass
 
 @dataclass
 class TPLinkRouterSensorRequiredKeysMixin:
-    value: Callable[[CombinedStatus], Any]
+    value: Callable[[Status,IPv4Status, LTEStatus], Any]
 
 @dataclass
 class TPLinkRouterSensorEntityDescription(
@@ -128,7 +125,7 @@ SENSOR_TYPES: tuple[TPLinkRouterSensorEntityDescription, ...] = (
         name="LTE Current RX Speed",
         icon="mdi:sim-outline",
         state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        native_unit_of_measurement=BYTES_PER_SECOND,
         value=lambda status: status.cur_rx_speed,
     ),
     TPLinkRouterSensorEntityDescription(
@@ -136,7 +133,7 @@ SENSOR_TYPES: tuple[TPLinkRouterSensorEntityDescription, ...] = (
         name="LTE Current TX Speed",
         icon="mdi:sim-outline",
         state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        native_unit_of_measurement=BYTES_PER_SECOND,
         value=lambda status: status.cur_tx_speed,
     ),
     TPLinkRouterSensorEntityDescription(
